454c22c04562877602e70384f450ce55689700f8
Jacob Jensen <jacob.jensen@sheknows.com>
2016-03-04 22:49:45 -0700

    Update youtube.go
    Update config

--- youtube.go
+++ youtube.go
@@ -10,7 +10,7 @@ import (
 	"code.google.com/p/google-api-go-client/youtube/v3"
 )
 
-const developerKey = "AIzaSyBntcD9JHQ56mAk-tjuixOCHqq6LPmPSqE"
+const developerKey = "DEVELOPER_KEY"
 
 // GetRandomWord generates a random word
 func GetRandomWord() string {
